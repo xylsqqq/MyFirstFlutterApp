d6a77ca6fb89fb850ccfa06d1e3fbda2d33c748e
xylsqqq <58775482+xylsqqq@users.noreply.github.com>
2022-06-25 01:50:01 +0900

    画像を表示するviewを追加、networkと画像ファイル読み込みの２方式で画像を表示 (#3)
    * 画像を表示するviewを追加、networkと画像ファイル読み込みの２方式で画像を表示
    
    * いらないimportとbuttonを削除、<Widget>を書く必要がないので削除

---
+++ lib/show_image_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ImagePage extends StatelessWidget {
+  final String _imageURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1cDnT1Q5ZrkfLfxiSgFvC2ZsjpngynJGvg&usqp=CAU';
+  const ImagePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image View'),
+      ),
+      body: Container(
+        color: Colors.black12,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: NetworkImage(_imageURL),
+            ),
+            Image.asset('images/cat.jpeg'),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                      '画像だよ',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  Icon(Icons.add_a_photo)
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
